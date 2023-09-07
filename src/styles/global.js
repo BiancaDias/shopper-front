40b1b7a031b181acbab616c9aa1776e766175576
Bianca Dias <bianca.dias@ufv.br>
2023-09-07 14:18:16 -0300

    feat: global style

---
+++ src/styles/global.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family:  Arial, Helvetica, sans-serif;
+    font-size: 14px;
+    background: #e8f3d8;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+`
+
+export default GlobalStyle
